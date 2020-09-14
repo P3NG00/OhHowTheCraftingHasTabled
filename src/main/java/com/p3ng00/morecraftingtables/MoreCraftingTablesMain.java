6d07394cc657bef95ffd84d5ebaa6676f3cac570
P3NG00 <p3ng00minecraft@gmail.com>
2020-09-14 17:52:23 -0600

    *

--- src/main/java/com/p3ng00/morecraftingtables/MoreCraftingTablesMain.java
+++ src/main/java/com/p3ng00/morecraftingtables/MoreCraftingTablesMain.java
@@ -1,9 +1,9 @@
 package com.p3ng00.morecraftingtables;
 
-import com.p3ng00.morecraftingtables.block.CraftingTableBlock;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.registry.FuelRegistry;
 import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
@@ -22,6 +22,7 @@ public class MoreCraftingTablesMain implements ModInitializer {
 
     @Override
     public void onInitialize() {
+
         registerCraftingTable(SPRUCE_CRAFTING_TABLE, "spruce_crafting_table", true);
         registerCraftingTable(BIRCH_CRAFTING_TABLE, "birch_crafting_table", true);
         registerCraftingTable(JUNGLE_CRAFTING_TABLE, "jungle_crafting_table", true);
@@ -29,17 +30,27 @@ public class MoreCraftingTablesMain implements ModInitializer {
         registerCraftingTable(DARK_OAK_CRAFTING_TABLE, "dark_oak_crafting_table", true);
         registerCraftingTable(CRIMSON_CRAFTING_TABLE, "crimson_crafting_table", false);
         registerCraftingTable(WARPED_CRAFTING_TABLE, "warped_crafting_table", false);
+
     }
 
-    private void registerCraftingTable(Block craftingTable, String name, boolean fuel) {
+    private void registerCraftingTable(Block craftingTable, String name, boolean canBurn) {
 
         Identifier id = new Identifier("morecraftingtables", name);
 
         Registry.register(Registry.BLOCK, id, craftingTable);
         Registry.register(Registry.ITEM, id, new BlockItem(craftingTable, new Item.Settings().group(ItemGroup.DECORATIONS)));
 
-        if (fuel)
+        if (canBurn)
             FuelRegistry.INSTANCE.add(craftingTable, 300);
 
     }
+
+    private static class CraftingTableBlock extends net.minecraft.block.CraftingTableBlock {
+
+        public CraftingTableBlock() {
+            super(Settings.copy(Blocks.CRAFTING_TABLE));
+        }
+
+    }
+
 }
--- src/main/java/com/p3ng00/morecraftingtables/mixin/CraftingScreenHandlerMixin.java
+++ src/main/java/com/p3ng00/morecraftingtables/mixin/CraftingScreenHandlerMixin.java
@@ -1,7 +1,6 @@
 package com.p3ng00.morecraftingtables.mixin;
 
-import com.p3ng00.morecraftingtables.MoreCraftingTablesMain;
-import net.minecraft.block.Blocks;
+import net.minecraft.block.CraftingTableBlock;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.CraftingInventory;
 import net.minecraft.screen.AbstractRecipeScreenHandler;
@@ -9,13 +8,17 @@ import net.minecraft.screen.CraftingScreenHandler;
 import net.minecraft.screen.ScreenHandlerContext;
 import net.minecraft.screen.ScreenHandlerType;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
-
-import java.util.stream.Stream;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(CraftingScreenHandler.class)
 public abstract class CraftingScreenHandlerMixin extends AbstractRecipeScreenHandler<CraftingInventory> {
+
+    /*
+     *  Taken from Paulevs 'Better Nether'
+     */
 
     @Shadow
     private ScreenHandlerContext context;
@@ -24,8 +27,11 @@ public abstract class CraftingScreenHandlerMixin extends AbstractRecipeScreenHan
         super(screenHandlerType, i);
     }
 
-    @Overwrite
-    public boolean canUse(PlayerEntity player) {
-        return Stream.of(Blocks.CRAFTING_TABLE, MoreCraftingTablesMain.SPRUCE_CRAFTING_TABLE, MoreCraftingTablesMain.BIRCH_CRAFTING_TABLE, MoreCraftingTablesMain.JUNGLE_CRAFTING_TABLE, MoreCraftingTablesMain.ACACIA_CRAFTING_TABLE, MoreCraftingTablesMain.DARK_OAK_CRAFTING_TABLE, MoreCraftingTablesMain.CRIMSON_CRAFTING_TABLE, MoreCraftingTablesMain.WARPED_CRAFTING_TABLE).anyMatch(block -> !canUse(this.context, player, block));
+    @Inject(at = @At("HEAD"), method = "canUse", cancellable = true)
+    public void canUse(PlayerEntity player, CallbackInfoReturnable<Boolean> info) {
+
+        info.setReturnValue(context.run((world, blockPos) -> world.getBlockState(blockPos).getBlock() instanceof CraftingTableBlock, true));
+        info.cancel();
+
     }
 }
